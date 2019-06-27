ec2a9655ce1de81438b6239339f1224805581cd5
Avinash <avisingh736@gmail.com>
2019-06-27 11:17:54 +0530

    LiveData implemented

--- app/src/main/java/com/devspot/androidjetpackexample/MainActivity.java
+++ app/src/main/java/com/devspot/androidjetpackexample/MainActivity.java
@@ -1,9 +1,11 @@
 package com.devspot.androidjetpackexample;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.devspot.androidjetpackexample.databinding.ActivityMainBinding;
@@ -12,16 +14,64 @@ import com.devspot.androidjetpackexample.viewmodels.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Handler mThreadHandler;
+    private Runnable mProfileManager1;
+    private Runnable mProfileManager2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        /**
+         * Here we set Profile object to binding
+         * */
         //binding.setProfile(getProfile());
 
-        MainViewModel mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        binding.setProfile(mainViewModel.getProfile());
 
+        final MainViewModel mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        /**
+         * Here we set Profile object to binding from ViewHolder
+         * */
+        //binding.setProfile(mainViewModel.getProfile());
+
+
+        /**
+         * Here we creating and an observer and listening it
+         * */
+        final Observer<Profile> mProfileObserver = new Observer<Profile>() {
+            @Override
+            public void onChanged(Profile profile) {
+                binding.setProfile(profile);
+            }
+        };
+        mainViewModel.getLiveProfile().observe(this,mProfileObserver);
+
+        mThreadHandler = new Handler();
+        mProfileManager1 = new Runnable() {
+            @Override
+            public void run() {
+                mainViewModel.getLiveProfile().setValue(new Profile("mDevSpot","mdevspot@gmail.com","8871608443","https://www.mdevspot.com/profile/mdevspot.jpg","25"));
+                mThreadHandler.postDelayed(mProfileManager2,1000);
+            }
+        };
+
+        mProfileManager2 = new Runnable() {
+            @Override
+            public void run() {
+                mainViewModel.getLiveProfile().setValue(new Profile("Avinash Kumar","avisingh736@gmail.com","8871608443","https://www.mdevspot.com/profile/avinash.jpg","25"));
+                mThreadHandler.postDelayed(mProfileManager1,1000);
+            }
+        };
+
+        mThreadHandler.post(mProfileManager1);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mThreadHandler.removeCallbacks(mProfileManager1);
+        mThreadHandler.removeCallbacks(mProfileManager2);
     }
 
     private Profile getProfile() {
--- app/src/main/java/com/devspot/androidjetpackexample/viewmodels/MainViewModel.java
+++ app/src/main/java/com/devspot/androidjetpackexample/viewmodels/MainViewModel.java
@@ -1,21 +1,40 @@
 package com.devspot.androidjetpackexample.viewmodels;
 
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.devspot.androidjetpackexample.models.Profile;
 
 public class MainViewModel extends ViewModel {
     private Profile profile;
-    public MainViewModel() {
-        profile = new Profile("mDevSpot","mdevspot@gmail.com","8871608443","https://www.mdevspot.com/profile/deep.jpg","25");
-    }
 
+    private MutableLiveData<Profile> mProfile;
+
+    /**
+     * This method is to get profile
+     * @return Profile
+     * Not observable
+     * */
     public Profile getProfile() {
+        if(profile == null) {
+            profile = new Profile("mDevSpot","mdevspot@gmail.com","8871608443","https://www.mdevspot.com/profile/deep.jpg","25");
+        }
         return profile;
     }
 
-    public void setProfile(Profile profile) {
-        this.profile = profile;
+
+    /**
+     * This method is to get livedata profile
+     * @return LiveData
+     * Not observable
+     * */
+    public MutableLiveData<Profile> getLiveProfile() {
+        if(mProfile == null) {
+            mProfile = new MutableLiveData<>();
+            mProfile.setValue(new Profile("mDevSpot","mdevspot@gmail.com","8871608443","https://www.mdevspot.com/profile/deep.jpg","25"));
+        }
+        return mProfile;
     }
 }
